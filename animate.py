021682da38bd454d9cd3234172a35ddaa44fdecf
Andrew Briscoe <everynothing@gmail.com>
2019-03-12 03:51:13 -0600

    changed turnon

---
+++ animate.py
@@ -0,0 +1,13 @@
+#from jeff import *
+from Images2Blox import *
+
+while True:
+
+    #b = blank_bloxl()
+
+    #for i in range(3):
+    #    test_func()
+
+    #b = blank_bloxl()
+
+    RunAnimations(1)
--- turnon.py
+++ turnon.py
@@ -3,11 +3,11 @@ from Images2Blox import *
 
 while True:
 
-    #b = blank_bloxl()
+    # b = blank_bloxl()
+    #
+    # for i in range(3):
+    #     test_func()
+    #
+    # b = blank_bloxl()
 
-    #for i in range(3):
-    #    test_func()
-
-    b = blank_bloxl()
-
-    RunAnimations(3)
+    RunAnimations(1)
